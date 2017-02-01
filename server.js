d01c3c1772b8f581d692e7db0f351266d74db2ba
Jonathan <jonmorazav@yahoo.com>
2017-02-01 20:16:23 +0000

    Added responsiveness, conversion options, and more front-end design

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const server = express();;
 const bodyParser = require('body-parser');
+const pd = require('pretty-data').pd;
 
 const hostname = process.env.IP;
 const port = process.env.PORT;
@@ -11,10 +12,19 @@ server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 
 
+
+
 server.get('/', function(req, res) {
     res.sendfile('platform/index.html')
 })
 
+server.post('/getXML', function(req, res) {
+    var unformattedxml = req.body.text;
+    console.log(unformattedxml)
+    var xml_pp = pd.xml(unformattedxml);
+    console.log(xml_pp);
+    res.send(xml_pp);
+})
 
 server.listen(port, function() {
   console.log('Server is running!');
